1d4637217089b1f5dad7461aa7a80428a1b41d39
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-03-18 15:56:22 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " integer numbers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxVal = arr[n - 1];
+    arr[n - 1] = -1;
+
+    for (int i = n - 2; i >= 0; i--) {
+        int temp = arr[i];
+        arr[i] = maxVal;
+        maxVal = max(maxVal, temp);
+    }
+
+    cout << "Resulting array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
