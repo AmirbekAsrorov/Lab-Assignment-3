2fc2e99299a1307f601b78a148404169e1e450ed
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-03-22 14:15:19 +0500

    Update problem6.cpp

--- problem6.cpp
+++ problem6.cpp
@@ -1,129 +1,42 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <climits>
 #include <cmath>
+
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
+int findMinimumDistance(const vector<int>& arr, int x, int y) {
+    int minDistance = INT_MAX;
+    int prevIndex = -1;
 
-    Node(int value) {
-        data = value;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-
-Node* insertNode(Node* root, int value) {
-    if (root == nullptr) {
-        return new Node(value);
-    }
-
-    queue<Node*> q;
-    q.push(root);
-
-    while (!q.empty()) {
-        Node* current = q.front();
-        q.pop();
-
-        if (current->left != nullptr) {
-            q.push(current->left);
-        } else {
-            current->left = new Node(value);
-            return root;
-        }
-
-        if (current->right != nullptr) {
-            q.push(current->right);
-        } else {
-            current->right = new Node(value);
-            return root;
-        }
-    }
-
-    return root;
-}
-
-int findLevel(Node* root, int value, int level) {
-    if (root == nullptr) {
-        return -1;
-    }
-
-    if (root->data == value) {
-        return level;
-    }
-
-    int leftLevel = findLevel(root->left, value, level + 1);
-    if (leftLevel != -1) {
-        return leftLevel;
-    }
-
-    return findLevel(root->right, value, level + 1);
-}
-
-int findDistance(Node* root, int x, int y) {
-    if (root == nullptr) {
-        return 0;
-    }
-
-    Node* commonAncestor = nullptr;
-    queue<Node*> q;
-    q.push(root);
-
-    while (!q.empty()) {
-        Node* current = q.front();
-        q.pop();
-
-        if ((current->data == x || current->data == y) && commonAncestor == nullptr) {
-            commonAncestor = current;
-        }
-
-        if (current->left != nullptr) {
-            q.push(current->left);
-        }
-
-        if (current->right != nullptr) {
-            q.push(current->right);
-        }
-
-        if (commonAncestor != nullptr) {
-            if (current->data == x || current->data == y) {
-                return abs(findLevel(root, x, 0) - findLevel(root, y, 0));
-            } else if (findLevel(current, x, 0) != -1 || findLevel(current, y, 0) != -1) {
-                int distanceX = findLevel(current, x, 0);
-                int distanceY = findLevel(current, y, 0);
-                return distanceX + distanceY;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == x || arr[i] == y) {
+            if (prevIndex != -1 && arr[i] != arr[prevIndex]) {
+                minDistance = min(minDistance, abs(i - prevIndex));
             }
+            prevIndex = i;
         }
     }
 
-    return 0;
+    return minDistance;
 }
-
 int main() {
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    Node* root = nullptr;
-
+    vector<int> arr(n);
     cout << "Enter " << n << " integer numbers: ";
     for (int i = 0; i < n; i++) {
-        int value;
-        cin >> value;
-        root = insertNode(root, value);
+        cin >> arr[i];
     }
 
     int x, y;
-    cout << "Enter two numbers (x and y): ";
+    cout << "Enter two numbers: ";
     cin >> x >> y;
+    int minDistance = findMinimumDistance(arr, x, y);
 
-    int distance = findDistance(root, x, y);
-
-    cout << "Minimum distance between " << x << " and " << y << " is: " << distance << endl;
+    cout <</* "Minimum distance between " << x << " and " << y << ": " <<*/ minDistance << endl;
 
     return 0;
 }
-
